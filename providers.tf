36dffe61c20a01f8f74779f15cdc53d31a2fd1c2
userforpython47 <userforpython47@wsl>
2024-08-19 17:24:26 -0500

    Update tfstate backend key

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket  = "terraform-aws-service-infra"
-    key     = "production/terraform.tfstate"
+    key     = "terraform-resources-project/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
